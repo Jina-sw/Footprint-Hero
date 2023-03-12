f356eb4e1391f6f60ce03dbc26476be805f332da
lizard777 <79130777+lizard777@users.noreply.github.com>
2023-03-12 01:50:37 -0800

    django-update-w/react

--- React-Django-Tutorial/footprint/api/models.py
+++ React-Django-Tutorial/footprint/api/models.py
@@ -4,28 +4,36 @@ import random
 
 # Create your models here.
 
-def generate_unique_code():
-    length = 6
-
-    while True: 
-        code = ''.join(random.choices(string.ascii_uppercase, k =length ))
-        #make sure that the code is unique
-        if Room.objects.filter(code = code).count() == 0:
-            break
-        return code
 
 #decide the fields we want in our room 
 #the pieces of information we want to store for each group
-class Room(models.Model):
+class Student(models.Model):
     #charfield stores a bunch of characters
     # max_length --> what is the maximum length the field can be 
     #can also set a default value as well
     #typing in the constraints of that specific field
-    code = models.CharField(max_length = 8, default = "",unique = True)
-    host = models.CharField(max_length = 50, unique = True )
-    guest_can_pause = models.BooleanField(null = False, default = False)
-    votes_to_skip = models.IntegerField(null = False, default = 2)
+    #electrical_bill = models.CharField(max_length = 8, default = "")
+    #gas_bill = models.CharField(max_length = 8, default = "")
+    #oil_bill = models.CharField(max_length = 8, default = "")
+    #mileage_bill = models.CharField(max_length = 8, default = "")
+    #flight_4_under = models.CharField(max_length = 8, default = "")
+    #flight_4_over = models.CharField(max_length = 8, default = "")
+
+   # newspaper = models.BooleanField(null = False, default = False)
+    #recycle = models.BooleanField(null = False, default = False)
+    
+    #code = models.CharField(max_length = 8, default = "",unique = True)
+    #host = models.CharField(max_length = 50, unique = True )
+    #guest_can_pause = models.BooleanField(null = False, default = False)
+    #votes_to_skip = models.IntegerField(null = False, default = 2)
     #whenever create a new room, it will automatically add the date and time 
+
+    name = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 254)
+    document = models.CharField(max_length = 20)
+    phone = models.CharField(max_length = 20)
+
+
     created_at = models.DateTimeField(auto_now_add = True)
 
     #can also add methods to the model
--- React-Django-Tutorial/footprint/api/views.py
+++ React-Django-Tutorial/footprint/api/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RoomSerializer
-from .models import Room
+from .models import Student
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_except 
+
+#@csrf_exempt
+def create_student(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        document = request.POST.get('document')
+        phone = request.POST.get('phone')
+
+        new_student = Student(name = name,email = email, document = document, phone = phone)
+        return JsonResponse({'message':'success'})
+    else:
+        return JsonResponse({'error':'invalid request method'})
+
 
 # Create your views here.
 
 #allows us to create a room and look at all the different types of rooms 
-class RoomView(generics.CreateAPIView):
+#class RoomView(generics.CreateAPIView):
     #returns all of the room objects 
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+ #   queryset = Room.objects.all()
+  #  serializer_class = RoomSerializer 
 
 
