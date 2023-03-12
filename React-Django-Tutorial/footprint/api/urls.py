cf913107bc4888321e5fcdae15ed869c5954bf76
lizard777 <79130777+lizard777@users.noreply.github.com>
2023-03-12 03:32:00 -0700

    django-finalfix-no

--- React-Django-Tutorial/footprint/api/urls.py
+++ React-Django-Tutorial/footprint/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import create_student
 
 urlpatterns = [
-    path('home',create_student.as_view())
+    path('home',create_student, name = 'create_student')
 ]
