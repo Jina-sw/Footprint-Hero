e6a996f732558e5a18377c4d43ada2fa4bea9500
Jina <77096179+Jina-sw@users.noreply.github.com>
2023-03-12 03:20:15 -0700

    dsf

--- src/Programs/HeroCal.js
+++ src/Programs/HeroCal.js
@@ -4,6 +4,65 @@ import './Program.css';
 import Nav from './Nav'
 
 function HeroCal(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [document, setDocument] = useState("");
+    const [phone, setPhone] = useState("");
+    // const [lessThan4, setLessThan4] = useState("");
+    // const [moreThan4, setMoreThan4] = useState("");
+    // const [recycle, setRecycle] = useState("");
+
+    // const num1 = () => {
+    //     setElectericBill(electericBill);
+    // }
+
+    // const num2 = () => {
+    //     setGasBill(gasBill);
+    // }
+    
+    // const num3 = () => {
+    //     setOilBill(oilBill);
+    // }
+
+    // const num4 = () => {
+    //     setMileageBill(mileageBill);
+    // }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = { name, email, document, phone };
+        fetch('http://localhost:8000/home', {
+            mode: 'no-cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            // body: JSON.stringify(data)
+            body: new URLSearchParams(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error)=>{
+            console.error('Error:', error)
+        });
+            
+    
+    }
+
+    // const num5 = () => {
+    //     setLessThan4(lessThan4);
+    // }
+
+    // const num6 = () => {
+    //     setMoreThan4(moreThan4);
+    // }
+
+    // const num7 = () => {
+    //     setRecycle(recycle);
+    // }
+
     return(
         <div className="m">
             <Nav></Nav>
@@ -13,14 +72,40 @@ function HeroCal(){
             <div className="tagline">
                Fill in the blanks below 
             </div>
-            <div className="scoreC">
-                <div className="score">
-                    1800
-                </div>
-            </div>
+            
+            <form onSubmit={handleSubmit}>
+
+
+                <input type="text" value={name} onChange= {e => setName(e.target.value)}  />
+                <input type="text" value={email} onChange= {e => setEmail(e.target.value)}  />
+                <input type="text" value={document} onChange= {e => setDocument(e.target.value)}  />
+                <input type="text" value={phone} onChange= {e => setPhone(e.target.value)}  />
+                {/* <input type="text" onchange= {field => setLessThan4(field.target.value)}  />
+                <input type="text" onchange= {field => setMoreThan4(field.target.value)}  /> */}
+
+                {/* <label>
+                    <input
+                        type="radio"
+                        value="YES"
+                        checked={recycle === 'YES'}
+                        onChange={setRecycle("YES")}
+                    />
+                    YES
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="NO"
+                        checked={recycle === 'NO'}
+                        onChange={setRecycle("NO")}
+                    />
+                    NO
+                </label> */}
+                <button type='submit'>press</button>
+            </form>
               
         </div>
-    )    
+    )
 }
 
 export default HeroCal;
