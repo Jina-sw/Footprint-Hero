5cfd7bc8d83c1f257761c5a03dce474521271dbd
lizard777 <79130777+lizard777@users.noreply.github.com>
2023-03-11 19:45:16 -0800

    playingaround with django

--- React-Django-Tutorial/footprint/api/views.py
+++ React-Django-Tutorial/footprint/api/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 def main(request):
     return HttpResponse("Hello")
+
+#can also use post delete for endpoints 
+@api_view(['GET'])
+def getData(request):
+    person = {'name':'Denis', 'age':28}
+    return Response(person)
--- React-Flask-Video/flask-server/server.py
+++
@@ -1,8 +0,0 @@
-print('hello world')
-
-
-
-
-def initial(x):
-	print("cool you got the function working")
-
