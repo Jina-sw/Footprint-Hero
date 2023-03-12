4547590efdfed626fb4ddc8e1ded335bf1b01a5d
Jina <77096179+Jina-sw@users.noreply.github.com>
2023-03-11 17:12:55 -0800

    main page UI

---
+++ src/Main.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Main.css';
+
+function main() {
+    return(
+        <div className="container">
+            <div className="logo">
+                Footprint Hero
+            </div>
+            <div className="filler"></div>
+            <div className="tagline">
+                We Fight for Greener World
+            </div>
+            <div className="bg">
+                <div className="bgLine">
+                    Let's get into it...
+                </div>
+                <div className="buttons">
+                    <button className="signin">Sign In</button>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default main;
